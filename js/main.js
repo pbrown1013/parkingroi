c55ba2db451f6be6b599be6851f205d9a242e56e
Parker Brown <parkerbrown1@me.com>
2020-02-02 04:54:04 -0600

    tedST

--- js/main.js
+++ js/main.js
@@ -31,7 +31,7 @@ var xhr = new XMLHttpRequest();
 
 xhr.onreadystatechange = function() {
   if (xhr.readyState === 4) {
-    console.log("its lit");
+    console.log(typeof xhr.responseText);
 
     // if (xhr.status === 200) {
     //   document.getElementById("ajax").innerHTML = xhr.responseText;
