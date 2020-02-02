c60a1b872eb1aa5f4ed2b21cabd5833f1bf1870a
Parker Brown <parkerbrown1@me.com>
2020-02-02 05:22:25 -0600

    tedST

--- js/main.js
+++ js/main.js
@@ -45,7 +45,8 @@ xhr.onreadystatechange = function() {
 };
 xhr.open(
   "POST",
-  "https://parkhub-bosh-poc.bubbleapps.io/version-test/api/1.1/wf/calc"
+  "https://parkhub-bosh-poc.bubbleapps.io/version-test/api/1.1/wf/calc",
+  true
 );
 xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
