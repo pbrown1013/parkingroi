77abaa22e3d15c80d67f1ddd79ab2bee23ba7963
Parker Brown <parkerbrown1@me.com>
2020-02-02 05:23:52 -0600

    tedST

--- js/main.js
+++ js/main.js
@@ -51,7 +51,7 @@ xhr.open(
 xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
 function sendAJAX() {
-  xhr.send(data);
+  xhr.send();
 }
 // let Headers = {
 //   "Content-Type": "application/x-www-form-urlencoded"
